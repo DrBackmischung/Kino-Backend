9ff6ac90e6b2d4ea1971e21bd655836563fbd4e3
DrBackmischung aka Mathis <mathis.neunzig@gmail.com>
2021-11-28 22:56:33 +0100

    test

--- src/test/java/de/wi2020sebgroup1/cinema/CinemaApplicationTests.java
+++ src/test/java/de/wi2020sebgroup1/cinema/CinemaApplicationTests.java
@@ -1,5 +1,7 @@
 package de.wi2020sebgroup1.cinema;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,9 @@ class CinemaApplicationTests {
 
 	@Test
 	void contextLoads() {
+		int i = 5;
+		int j = 5;
+		assertEquals(i,  j);
 	}
 
 }
