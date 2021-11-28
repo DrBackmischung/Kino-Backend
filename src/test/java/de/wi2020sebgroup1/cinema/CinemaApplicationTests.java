08ff40161216a36271c253ec244a77a1eb0526c8
DrBackmischung aka Mathis <mathis.neunzig@gmail.com>
2021-11-28 21:20:01 +0100

    initialize backend

---
+++ src/test/java/de/wi2020sebgroup1/cinema/CinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package de.wi2020sebgroup1.cinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
