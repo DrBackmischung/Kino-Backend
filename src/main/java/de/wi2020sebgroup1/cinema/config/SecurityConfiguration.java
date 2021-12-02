a78ed7d34d49a519dffae831c4e783c153bab781
DrBackmischung aka Mathis <mathis.neunzig@gmail.com>
2021-12-02 18:05:09 +0100

    fix

--- src/main/java/de/wi2020sebgroup1/cinema/config/SecurityConfiguration.java
+++ src/main/java/de/wi2020sebgroup1/cinema/config/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -17,6 +18,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled=true,proxyTargetClass=true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
