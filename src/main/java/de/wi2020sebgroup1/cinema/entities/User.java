9de3cbdb61ef5ee4a28b94603e549285ca32af43
DrBackmischung aka Mathis <mathis.neunzig@gmail.com>
2021-12-04 22:21:37 +0100

    getter/setter

--- src/main/java/de/wi2020sebgroup1/cinema/entities/User.java
+++ src/main/java/de/wi2020sebgroup1/cinema/entities/User.java
@@ -58,6 +58,30 @@ public class User {
 		this.role = role;
 	}
 
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
