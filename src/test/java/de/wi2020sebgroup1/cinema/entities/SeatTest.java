06a2c006cb9064b2c2bc61d613ed6155472a0c84
Mathis Neunzig <mathis.neunzig@gmail.com>
2021-12-08 18:43:47 +0100

    KINO-120 (#14)
    * Optimize Tests
    
    * CinemaTest Optimierung
    
    * fix

--- src/test/java/de/wi2020sebgroup1/cinema/entities/SeatTest.java
+++ src/test/java/de/wi2020sebgroup1/cinema/entities/SeatTest.java
@@ -23,6 +23,28 @@ public class SeatTest {
     }
 	
 	@Test
+	@DisplayName("Test Getter/Setter")
+    public void testGetterSetter() {
+		CinemaRoomSeatingPlan c = new CinemaRoomSeatingPlan(0);
+		Show s = new Show(null, null, null, null, null, null);
+		Seat o = new Seat(1, 3, true, true, 20, c, s);
+		o.setReihe(0);
+        assertEquals(o.getReihe(), 0);
+        o.setPlace(0);
+        assertEquals(o.getPlace(), 0);
+        o.setBlocked(false);
+        assertEquals(o.isBlocked(), false);
+        o.setCoupleSeat(false);
+        assertEquals(o.isCoupleSeat(), false);
+        o.setSurcharge(10);
+        assertEquals(o.getSurcharge(), 10);
+        o.setCinemaRoomSeatingPlan(null);
+        assertEquals(o.getCinemaRoomSeatingPlan(), null);
+        o.setShow(null);
+        assertEquals(o.getShow(), null);
+    }
+	
+	@Test
 	@DisplayName("Equals consistency")
     public void testCompare() {
 		CinemaRoomSeatingPlan c = new CinemaRoomSeatingPlan(0);
@@ -31,6 +53,33 @@ public class SeatTest {
 		Seat o2 = new Seat(1, 3, true, true, 20, c, s);
 		assertEquals(o.hashCode(), o2.hashCode());
 		assertEquals(o.equals(o2), true);
+    }
+	
+	@SuppressWarnings("unlikely-arg-type")
+	@Test
+	@DisplayName("Equals inconsistency fail")
+    public void testCompareFail() {
+		CinemaRoomSeatingPlan c = new CinemaRoomSeatingPlan(0);
+		Show s = new Show(null, null, null, null, null, null);
+		Seat o = new Seat(1, 3, true, true, 20, c, s);
+		Seat o2 = new Seat(1, 4, true, true, 20, c, s);
+		Seat o3 = new Seat(1, 3, false, true, 20, c, s);
+		Seat o4 = new Seat(1, 3, true, false, 20, c, s);
+		Seat o5 = new Seat(1, 3, true, true, 30, c, s);
+		Seat o6 = new Seat(1, 3, true, true, 20, null, s);
+		Seat o7 = new Seat(1, 3, true, true, 20, c, null);
+		Seat o8 = new Seat(2, 3, true, true, 20, c, s);
+		Seat o9 = null;
+		String st = "Test";
+		assertEquals(o.equals(o2), false);
+		assertEquals(o.equals(o3), false);
+		assertEquals(o.equals(o4), false);
+		assertEquals(o.equals(o5), false);
+		assertEquals(o.equals(o6), false);
+		assertEquals(o.equals(o7), false);
+		assertEquals(o.equals(o8), false);
+		assertEquals(o.equals(o9), false);
+		assertEquals(o.equals(st), false);
     }
 	
 }
--- src/test/java/de/wi2020sebgroup1/cinema/entities/ShowTest.java
+++ src/test/java/de/wi2020sebgroup1/cinema/entities/ShowTest.java
@@ -28,6 +28,29 @@ public class ShowTest {
     }
 	
 	@Test
+	@DisplayName("Test Getter/Setter")
+    public void testGetterSetter() {
+		Movie m = new Movie(null, null, 0, null, null, null);
+		Cinema c = new Cinema(null, null, null, 0, 0, null);
+		CinemaRoom c2 = new CinemaRoom(0, false, c, null);
+		Date d = new Date(0);
+		Time t = new Time(0), t2 = new Time(0);
+		Show o = new Show(d, t, t2, m, c, c2);
+		o.setShowDate(null);
+        assertEquals(o.getShowDate(), null);
+        o.setStartTime(null);
+        assertEquals(o.getStartTime(), null);
+        o.setEndTime(null);
+        assertEquals(o.getEndTime(), null);
+        o.setMovie(null);
+        assertEquals(o.getMovie(), null);
+        o.setCinema(null);
+        assertEquals(o.getCinema(), null);
+        o.setCinemaRoom(null);
+        assertEquals(o.getCinemaRoom(), null);
+    }
+	
+	@Test
 	@DisplayName("Equals consistency")
     public void testCompare() {
 		Movie m = new Movie(null, null, 0, null, null, null);
@@ -39,6 +62,34 @@ public class ShowTest {
 		Show o2 = new Show(d, t, t2, m, c, c2);
 		assertEquals(o.hashCode(), o2.hashCode());
 		assertEquals(o.equals(o2), true);
+    }
+	
+	@SuppressWarnings("unlikely-arg-type")
+	@Test
+	@DisplayName("Equals inconsistency fail")
+    public void testCompareFail() {
+		Movie m = new Movie(null, null, 0, null, null, null);
+		Cinema c = new Cinema(null, null, null, 0, 0, null);
+		CinemaRoom c2 = new CinemaRoom(0, false, c, null);
+		Date d = new Date(0);
+		Time t = new Time(0), t2 = new Time(0);
+		Show o = new Show(d, t, t2, m, c, c2);
+		Show o2 = new Show(null, t, t2, m, c, c2);
+		Show o3 = new Show(d, null, t2, m, c, c2);
+		Show o4 = new Show(d, t, null, m, c, c2);
+		Show o5 = new Show(d, t, t2, null, c, c2);
+		Show o6 = new Show(d, t, t2, m, null, c2);
+		Show o7 = new Show(d, t, t2, m, c, null);
+		Show o8 = null;
+		String s = "Test";
+		assertEquals(o.equals(o2), false);
+		assertEquals(o.equals(o3), false);
+		assertEquals(o.equals(o4), false);
+		assertEquals(o.equals(o5), false);
+		assertEquals(o.equals(o6), false);
+		assertEquals(o.equals(o7), false);
+		assertEquals(o.equals(o8), false);
+		assertEquals(o.equals(s), false);
     }
 	
 }
