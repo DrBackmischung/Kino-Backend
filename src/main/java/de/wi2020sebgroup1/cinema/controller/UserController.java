9042d5e3a290ea924ed8cea4fde5d852b3e10875
DrBackmischung aka Mathis <mathis.neunzig@gmail.com>
2021-12-12 16:04:10 +0100

    UserController Test

--- src/main/java/de/wi2020sebgroup1/cinema/controller/UserController.java
+++ src/main/java/de/wi2020sebgroup1/cinema/controller/UserController.java
@@ -1,23 +1,21 @@
 package de.wi2020sebgroup1.cinema.controller;
 
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import de.wi2020sebgroup1.cinema.entities.Cinema;
-import de.wi2020sebgroup1.cinema.entities.City;
 import de.wi2020sebgroup1.cinema.entities.User;
-import de.wi2020sebgroup1.cinema.repositories.CinemaRepository;
-import de.wi2020sebgroup1.cinema.repositories.CityRepository;
 import de.wi2020sebgroup1.cinema.repositories.UserRepository;
 
 @Controller
@@ -31,6 +29,21 @@ public class UserController {
 	@GetMapping("/getAll")
 	public ResponseEntity<Iterable<User>> getUsers(){
 		return new ResponseEntity<>(userRepository.findAll(), HttpStatus.OK);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Object> getSpecific(@PathVariable UUID id){
+		
+		Optional<User> user = userRepository.findById(id);
+		
+		try {
+			User toReturn = user.get();
+			return new ResponseEntity<Object>(toReturn, HttpStatus.OK);
+		}
+		catch(NoSuchElementException e) {
+			return new ResponseEntity<Object>(new String("No user with id \"" + id + "\" found!"), HttpStatus.NOT_FOUND);
+		}
+		
 	}
 	
 	@PutMapping("/add")
