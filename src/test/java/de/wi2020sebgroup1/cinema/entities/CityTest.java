5b39b45412f81020b5aa941606b1f8d1a30042c6
DrBackmischung aka Mathis <mathis.neunzig@gmail.com>
2021-12-22 14:31:42 +0100

    City Test Hash

--- src/test/java/de/wi2020sebgroup1/cinema/entities/CityTest.java
+++ src/test/java/de/wi2020sebgroup1/cinema/entities/CityTest.java
@@ -2,6 +2,8 @@ package de.wi2020sebgroup1.cinema.entities;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.UUID;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -28,10 +30,17 @@ public class CityTest {
 	@Test
 	@DisplayName("Equals consistency")
     public void testCompare() {
+		UUID u = new UUID(2,2);
 		City o = new City(68159, "Mannheim");
+		o.setId(u);
 		City o2 = new City(68159, "Mannheim");
+		o2.setId(u);
 		assertEquals(o.hashCode(), o2.hashCode());
 		assertEquals(o.equals(o2), true);
+		City o3 = new City(68159, null);
+		City o4 = new City(68159, null);
+		assertEquals(o3.hashCode(), o4.hashCode());
+		assertEquals(o3.equals(o4), true);
     }
 	
 	@SuppressWarnings("unlikely-arg-type")
