05d135374f742bc259e7f25645ac9d0945b2a881
DrBackmischung aka Mathis <mathis.neunzig@gmail.com>
2021-12-22 13:56:52 +0100

    100% Cinema Test

--- src/test/java/de/wi2020sebgroup1/cinema/entities/CinemaTest.java
+++ src/test/java/de/wi2020sebgroup1/cinema/entities/CinemaTest.java
@@ -2,6 +2,8 @@ package de.wi2020sebgroup1.cinema.entities;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.UUID;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -45,8 +47,15 @@ public class CinemaTest {
 		City c = new City(0, null);
 		Cinema o = new Cinema("Kino Mannheim", "Q6", "14", 5, 2, c);
 		Cinema o2 = new Cinema("Kino Mannheim", "Q6", "14", 5, 2, c);
+		Cinema o3 = new Cinema(null, null, null, 5, 2, null);
+		o3.setId(null);
+		Cinema o4 = new Cinema(null, null, null, 5, 2, null);
+		o4.setId(null);
+		
 		assertEquals(o.hashCode(), o2.hashCode());
 		assertEquals(o.equals(o2), true);
+		assertEquals(o3.hashCode(), o4.hashCode());
+		assertEquals(o3.equals(o4), true);
     }
 	
 	@SuppressWarnings({ "unlikely-arg-type" })
@@ -54,31 +63,41 @@ public class CinemaTest {
 	@DisplayName("Equals inconsistency fail")
     public void testCompareFail() {
 		City c = new City(0, null);
-		Cinema o = new Cinema(null, null, null, 5, 2, null);
-		Cinema o2 = new Cinema("Kino Mannheim2", "Q6", "14", 5, 2, c);
-		Cinema o22 = new Cinema(null, "Q6", "14", 5, 2, c);
-		Cinema o3 = new Cinema("Kino Mannheim", "Q7", "14", 5, 2, c);
-		Cinema o32 = new Cinema(null, "Strasse", null, 5, 2, null);
-		Cinema o4 = new Cinema("Kino Mannheim", "Q6", "14A", 5, 2, c);
-		Cinema o42 = new Cinema(null, null, "Nr", 5, 2, null);
-		Cinema o5 = new Cinema("Kino Mannheim", "Q6", "14", 6, 2, c);
-		Cinema o6 = new Cinema("Kino Mannheim", "Q6", "14", 5, 1, c);
-		Cinema o7 = new Cinema("Kino Mannheim", "Q6", "14", 5, 1, null);
-		Cinema o72 = new Cinema(null, null, null, 5, 2, null);
-		Cinema o8 = null;
+		Cinema o = new Cinema("Kino Mannheim", "Q6", "14", 5, 2, c);
+		o.setId(new UUID(2,2));
+		assertEquals(o.equals(o), true);
+		Cinema o0 = new Cinema(null, null, null, 5, 2, null);
+		assertEquals(o0.equals(o0), true);
+		Cinema n = null;
+		assertEquals(o.equals(n), false);
 		String s = "Test";
-		assertEquals(o.equals(o2), false);
-		assertEquals(o.equals(o3), false);
-		assertEquals(o.equals(o4), false);
-		assertEquals(o.equals(o5), false);
-		assertEquals(o.equals(o6), false);
-		assertEquals(o.equals(o7), false);
-		assertEquals(o.equals(o8), false);
 		assertEquals(o.equals(s), false);
-		assertEquals(o.equals(o22), false);
-		assertEquals(o.equals(o32), false);
-		assertEquals(o.equals(o42), false);
-		assertEquals(o.equals(o72), true);
+		Cinema o2 = new Cinema("Kino Mannheim", "Q6", "14", 4, 2, c);
+		assertEquals(o.equals(o2), false);
+		Cinema o3 = new Cinema("Kino Mannheim", "Q6", "14", 5, 2, null);
+		Cinema o03 = new Cinema("Kino Mannheim", "Q6", "14", 5, 2, c);
+		assertEquals(o.equals(o3), false);
+		assertEquals(o0.equals(o03), false);
+		Cinema o4 = new Cinema("Kino Mannheim", "Q6", "14", 5, 2, c);
+		Cinema o04 = new Cinema("Kino Mannheim", "Q6", "14", 5, 2, c);
+		o4.setId(null);
+		o04.setId(new UUID(4,4));
+		assertEquals(o.equals(o4), false);
+		assertEquals(o0.equals(o04), false);
+		Cinema o5 = new Cinema(null, "Q6", "14", 5, 2, c);
+		Cinema o05 = new Cinema("Kino Mannheim", "Q6", "14", 5, 2, c);
+		assertEquals(o.equals(o5), false);
+		assertEquals(o0.equals(o05), false);
+		Cinema o6 = new Cinema("Kino Mannheim", "Q6", null, 5, 2, c);
+		Cinema o06 = new Cinema("Kino Mannheim", "Q6", "14", 5, 2, c);
+		assertEquals(o.equals(o6), false);
+		assertEquals(o0.equals(o06), false);
+		Cinema o7 = new Cinema("Kino Mannheim", "Q6", "14", 5, 1, c);
+		assertEquals(o.equals(o7), false);
+		Cinema o8 = new Cinema("Kino Mannheim", null, "14", 5, 2, c);
+		Cinema o08 = new Cinema("Kino Mannheim", "Q6", "14", 5, 2, c);
+		assertEquals(o.equals(o8), false);
+		assertEquals(o0.equals(o08), false);
     }
 	
 }
