160b265029858936cd0005a25958527eae59fe18
Neunzig <Neunzig@DESKTOP-GJFDB1H.fritz.box>
2021-12-25 15:18:39 +0100

    Create UserService with resolve()

---
+++ src/main/java/de/wi2020sebgroup1/cinema/services/UserService.java
@@ -0,0 +1,30 @@
+package de.wi2020sebgroup1.cinema.services;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import de.wi2020sebgroup1.cinema.entities.User;
+import de.wi2020sebgroup1.cinema.repositories.UserRepository;
+
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public User resolve(UUID userID) {
+		
+		Optional<User> user = userRepository.findById(userID);
+		
+		try {
+			return user.get();
+		} catch(NoSuchElementException e) {
+			e.printStackTrace();
+			return null;
+		}
+		
+	}
+	
+}
