779afedd2370beded0beaa5815a017a2117001cb
DrBackmischung aka Mathis <mathis.neunzig@gmail.com>
2022-01-07 20:31:29 +0100

    Repo

--- src/main/java/de/wi2020sebgroup1/cinema/repositories/BookingRepositroy.java
+++ src/main/java/de/wi2020sebgroup1/cinema/repositories/BookingRepositroy.java
@@ -1,12 +1,17 @@
 package de.wi2020sebgroup1.cinema.repositories;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.repository.CrudRepository;
 
 import de.wi2020sebgroup1.cinema.entities.Booking;
+import de.wi2020sebgroup1.cinema.entities.User;
 
 
 public interface BookingRepositroy extends CrudRepository<Booking, UUID> {
+	
+	Optional<List<Booking>> findAllByUser(User user);
 
 }
