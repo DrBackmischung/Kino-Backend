c68e385f145da46e73627029a1dbe40263818632
Mathis Neunzig <mathis.neunzig@gmail.com>
2022-01-07 23:14:40 +0100

    Kino-193 (#41)
    * News Entity
    
    * Revert "News Entity"
    
    This reverts commit 773d9a8180b05f8599445d68a5d6d46aa25a0bf1.
    
    * add Controller
    
    * Profile Controller
    
    * commit badge
    
    * fix
    
    * test fix
    
    * wait
    
    * commit badge
    
    * Repo
    
    * Booking umstellen
    
    * fix
    
    * test fix
    
    * commit badge
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/main/java/de/wi2020sebgroup1/cinema/repositories/TicketRepository.java
+++ src/main/java/de/wi2020sebgroup1/cinema/repositories/TicketRepository.java
@@ -1,11 +1,16 @@
 package de.wi2020sebgroup1.cinema.repositories;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.repository.CrudRepository;
 
 import de.wi2020sebgroup1.cinema.entities.Ticket;
+import de.wi2020sebgroup1.cinema.entities.User;
 
 public interface TicketRepository extends CrudRepository<Ticket, UUID> {
+	
+	Optional<List<Ticket>> findAllByUser(User user);
 
 }
