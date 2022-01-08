1ce36c89917a77cf4e80096997d44b747ce20ada
DrBackmischung aka Mathis <mathis.neunzig@gmail.com>
2022-01-08 23:59:55 +0100

    EmailService Test

--- src/test/java/de/wi2020sebgroup1/cinema/services/EmailServiceTest.java
+++ src/test/java/de/wi2020sebgroup1/cinema/services/EmailServiceTest.java
@@ -1,31 +1,24 @@
 package de.wi2020sebgroup1.cinema.services;
 
-import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.isA;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.times;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import java.util.Properties;
 
 import javax.mail.Authenticator;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
-import javax.mail.internet.MimeMessage;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @TestPropertySource(locations="classpath:test.properties")
 public class EmailServiceTest {
-	
-	@MockBean
-	HTMLService htmlService;
 
-    @MockBean
+	@Autowired
     EmailService emailService;
 	
 	@Test
@@ -42,21 +35,23 @@ public class EmailServiceTest {
                 return new PasswordAuthentication("wwi2020seb@gmail.com", "Kino2020SEB");
             }
         });
-    	when(emailService.prepareMessage(session, "wwi2020seb@gmail.com", "mathis.neunzig@gmail.com", "Registration completed!", "DrBackmischung", "Registration.html")).thenReturn(new MimeMessage(session));
-    	emailService.prepareMessage(null, null, null, null, null, null);
+		assertDoesNotThrow(new Executable() {
+            @Override
+            public void execute() {
+            	emailService.prepareMessage(session, "wwi2020seb@gmail.com", "mathis.neunzig@gmail.com", "Registration completed!", "DrBackmischung", "Registration.html");          
+            }
+        });
     	
 	}
 	
 	@Test
 	void testSendMessage() {
-		
-		try {
-			doNothing().when(emailService).sendMail(isA(String.class), isA(String.class), isA(String.class), isA(String.class));
-			emailService.sendMail("mathis.neunzig@gmail.com", "Registration completed!", "DrBackmischung", "Registration.html");
-			verify(emailService, times(1)).sendMail("mathis.neunzig@gmail.com", "Registration completed!", "DrBackmischung", "Registration.html");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		assertDoesNotThrow(new Executable() {
+            @Override
+            public void execute() throws Exception {
+            	emailService.sendMail("wwi2020seb@gmail.com", "Test :3", "DrBackmischung", "Registration.html");
+            }
+        });
 		
 	}
 	
