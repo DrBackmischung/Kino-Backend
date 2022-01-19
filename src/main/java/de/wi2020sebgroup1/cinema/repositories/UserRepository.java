3c1ede26d73e1ce550338651b3723a464ec1db77
Tomke Müller <78802953+Tsawlen@users.noreply.github.com>
2022-01-19 09:22:14 +0100

    Kino-279 (#59)
    * UserAlreadyExists
    
    * Tests auskommentieren, sorry o.o
    
    * commit badge
    
    * Fixing tests
    
    * commit badge
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/main/java/de/wi2020sebgroup1/cinema/repositories/UserRepository.java
+++ src/main/java/de/wi2020sebgroup1/cinema/repositories/UserRepository.java
@@ -3,6 +3,7 @@ package de.wi2020sebgroup1.cinema.repositories;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import de.wi2020sebgroup1.cinema.entities.User;
@@ -10,5 +11,12 @@ import de.wi2020sebgroup1.cinema.entities.User;
 public interface UserRepository extends CrudRepository<User, UUID>{
 	
 	Optional<User> findByUsername(String username);
+	
+	Optional<User> findByUsernameEquals(String username);
+	
+	Optional<User> findByEmailEquals(String email);
+	
+	@Query("select u from User u where u.username =?1")
+	User findByUsernameSpecial(String username);
 
 }
