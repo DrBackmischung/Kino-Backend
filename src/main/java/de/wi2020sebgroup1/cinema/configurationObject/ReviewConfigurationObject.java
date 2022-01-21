5a589bfd81905ef711118be5a3ed6607f4457b09
Mathis Neunzig <mathis.neunzig@gmail.com>
2022-01-21 01:08:06 +0100

    Kino-281 (#64)
    * Ratings
    
    * fix
    
    * fix
    
    * omg ich bin dumm

--- src/main/java/de/wi2020sebgroup1/cinema/configurationObject/ReviewConfigurationObject.java
+++ src/main/java/de/wi2020sebgroup1/cinema/configurationObject/ReviewConfigurationObject.java
@@ -10,15 +10,17 @@ public class ReviewConfigurationObject {
 	public Time time;
 	public String header;
 	public String content;
+	public int rating;
 	public UUID movieID;
 	public UUID userID;
 	
-	public ReviewConfigurationObject(Date date, Time time, String header, String content, UUID movieID, UUID userID) {
+	public ReviewConfigurationObject(Date date, Time time, String header, String content, int rating, UUID movieID, UUID userID) {
 		super();
 		this.date = date;
 		this.time = time;
 		this.header = header;
 		this.content = content;
+		this.rating = rating;
 		this.movieID = movieID;
 		this.userID = userID;
 	}
